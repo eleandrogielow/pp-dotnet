eb2676ce7f2dbd474722a7bb4c2a7da447ae90d8
eleandrogielow <eleandrogielow3@gmail.com>
2024-02-29 15:08:22 -0300

    Projeto prático dio com dotnet

---
+++ Clasees/SerieRepositorio.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using Dio.Series.interfaces;
+
+namespace Dio.Series
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>();
+        public void Atualizar(int id, Serie objeto)
+        {
+            listaSerie[id] = objeto;
+        }
+
+        public void Exclui(int id)
+        {
+            listaSerie[id].Excluir();
+            // implemento o envio de e-mail
+        }
+
+        public void Insere(Serie objeto)
+        {
+            listaSerie.Add(objeto);
+        }
+
+        public List<Serie> Lista()
+        {
+            return listaSerie;
+        }
+
+        public int ProximoId()
+        {
+            return listaSerie.Count;
+        }
+
+        public Serie RetornarPorId(int id)
+        {
+            return listaSerie[id];
+        }
+    }
+}
---
+++ README.md.txt
@@ -0,0 +1,18 @@
+## Digital Innovation One - Prática .NET
+
+### *Recriando um app básico de cadastro em memória*
+
+---------------------------------------------------------------------------------------------------------------
+
+- ####  Com o auxílio da video-aula, recriei este aplicativo afim de projeto para aula do bootcamp MRV .NET Developer.
+
+- #### Para fins didáticos sem a intenção de implementação imediata.
+
+  
+
+
+
+
+
+
+
